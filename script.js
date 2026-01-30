f476933fb4cec560ef394efd3b5f94905a0945a4
527mbeard <527mbeard@frhsd.com>
2026-01-30 10:43:53 -0500

    Add initial HTML, CSS, and JavaScript files for Rock-Paper-Scissors game

---
+++ script.js
@@ -0,0 +1,16 @@
+// page loads,
+// then save html elements as variables, 
+// then setup other variables (userScore =0,compScore=0, userName)
+// then get user name from prompt or text box with btn
+// then when userChoice buttons are clicked 
+// when rock btn is clicked, set user choice to rock
+// then get computer choice (function randomized) 
+// then evaluate of the round (function)
+// then points
+// then display results
+// then check game over()
+//D2B48C tan
+//FFF5E1 ivory mist
+//ADD8E6 light blue
+//FFDCC2 peach fuzz
+//FFB6B9 cherry blossom
